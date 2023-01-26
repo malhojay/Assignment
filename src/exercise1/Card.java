14ee7cf676508ce5f5dbc949e92769b4e079733a
malhojay <malhojay@sheridancollege.ca>
2023-01-25 21:44:00 -0500

    v01

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -1,21 +1,15 @@
 package exercise1;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
- * @author dancye
- * @author Paul Bonenfant May 2020
+ * @author Jayesh Malhotra 24 Jan 2023
  */
-public class Card {
+public class Card 
+{
 
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+   public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
      * @return the suit
      */
--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,46 +1,45 @@
 package exercise1;
+import java.util.Scanner;
 
 /**
- * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
- * To be used as starting code in Exercise
- *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Jayesh Malhotra 24 Jan 2023 
  */
-public class CardTrick {
-    
-    public static void main(String[] args) {
-        
+
+public class CardTrick 
+{    
+    public static void main(String[] args) 
+    {    
         Card[] hand = new Card[7];
 
-        for (int i = 0; i < hand.length; i++) {
+        for(int i = 0; i < hand.length; i++) 
+        {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue((int)(Math.random()*14));
+            card.setSuit(Card.SUITS[(int)(Math.random()*14)]);
+            hand[i] = card;
         }
-
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
         
-        // If the guess is successful, print System.out.println("Congratulations, you guessed right!");.
+        Scanner input = new Scanner(System.in);
+        int val;
+        String sui;  
+        System.out.print("Enter Card Value and Suit with a gap : "); 
+        val = input.nextInt();
+        sui = input.nextLine();
         
+        for(int j = 0; j < hand.length; j++)
+                {
+                    if(hand[j].getValue() == val && hand[j].getSuit() == sui)
+                    {
+                        printInfo();
+                    }
+                }  
     }
 
     /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
+     * @author Jayesh Malhotra 24 Jan 2023 
      */
-    private static void printInfo() {
+    private static void printInfo() 
+    {
     
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
@@ -60,8 +59,6 @@ public class CardTrick {
         System.out.println("-- Riding my motorcycle");
 
         System.out.println();
-        
-    
     }
 
 }
